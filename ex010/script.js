5ae0167563c2867bf09ee7b67befee437f3ca229
Giovana Victória <giovanavictoria887@gmail.com>
2022-05-13 22:45:45 -0300

    done

---
+++ ex010/script.js
@@ -0,0 +1,17 @@
+let resultado = window.document.getElementById('resultado')
+
+function um() {
+    resultado.innerHTML += `<p>Clicou no primeiro botão</p>`
+}
+
+function dois() {
+    resultado.innerHTML += `<p>Clicou no segundo botão</p>`
+}
+
+function tres() {
+    resultado.innerHTML += `<p>Clicou no terceiro botão</p>`
+}
+
+function quatro() {
+    resultado.innerHTML += `<p>Clicou no quarto botão</p>`
+}
